3c1d67e74c1cf523243d6b084793e15230cc0da3
JonhNino <jonhninoa@gmail.com>
2023-04-10 22:37:20 -0500

    Mintic-Crud.

---
+++ src/main/java/com/minticrud/app/MinticCrudApplication.java
@@ -0,0 +1,13 @@
+package com.minticrud.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinticCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinticCrudApplication.class, args);
+	}
+
+}
